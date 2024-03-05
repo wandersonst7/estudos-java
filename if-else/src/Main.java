a6b998462891976c8249cfd1aa241a7c7c468806
Wanderson <121998974+wandersonst7@users.noreply.github.com>
2024-03-05 11:29:36 -0300

    Condicional IF

---
+++ if-else/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int idade = 18;
+
+        if(idade >= 18) {
+            System.out.println("Você é um adulto.");
+        }else{
+            System.out.println("Você não é um adulto.");
+        }
+
+    }
+}
